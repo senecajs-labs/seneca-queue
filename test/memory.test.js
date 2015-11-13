8d5ea621467c948a799400c95cdd0e112dea6a83
Paolo Chiodi <chiodi84@gmail.com>
2015-11-13 16:21:43 +0100

    small test refactoring

--- test/memory.test.js
+++ test/memory.test.js
@@ -10,93 +10,95 @@ var it = lab.it
 var beforeEach = lab.beforeEach
 
 describe('seneca queue', function () {
-  var s
+  describe('Basics', function () {
+    var s
 
-  beforeEach(function (done) {
-    s = seneca({log: 'silent'})
-    s.use(queue)
-    s.ready(done)
-  })
-
-  it('should process a task', function (done) {
-    var task = {
-      task: 'my task',
-      param: 42
-    }
-
-    s.add({
-      task: 'my task'
-    }, function (args, cb) {
-      expect(args).to.include(task)
-      cb()
-      done()
+    beforeEach(function (done) {
+      s = seneca({log: 'silent'})
+        .use(queue)
+        .ready(done)
     })
 
-    s.act({ role: 'queue', cmd: 'start' }, function (err) {
-      if (err) { return done(err) }
-      s.act({ role: 'queue', cmd: 'enqueue', msg: task }, function (err) {
+    it('should process a task', function (done) {
+      var task = {
+        task: 'my task',
+        param: 42
+      }
+
+      s.add({
+        task: 'my task'
+      }, function (args, cb) {
+        expect(args).to.include(task)
+        cb()
+        done()
+      })
+
+      s.act({ role: 'queue', cmd: 'start' }, function (err) {
         if (err) { return done(err) }
+        s.act({ role: 'queue', cmd: 'enqueue', msg: task }, function (err) {
+          if (err) { return done(err) }
+        })
       })
     })
-  })
 
-  it('should not process a task until start is called', function (done) {
-    var task = {
-      task: 'my task',
-      param: 42
-    }
+    it('should not process a task until start is called', function (done) {
+      var task = {
+        task: 'my task',
+        param: 42
+      }
 
-    // first enqueing
-    s.act({ role: 'queue', cmd: 'enqueue', msg: task })
+      // first enqueing
+      s.act({ role: 'queue', cmd: 'enqueue', msg: task })
 
-    // then we add the task handler
-    s.add({
-      task: 'my task'
-    }, function (args, cb) {
-      expect(args).to.include(task)
-      cb()
-      done()
+      // then we add the task handler
+      s.add({
+        task: 'my task'
+      }, function (args, cb) {
+        expect(args).to.include(task)
+        cb()
+        done()
+      })
+
+      // finally we start the worker
+      s.act({ role: 'queue', cmd: 'start' })
     })
 
-    // finally we start the worker
-    s.act({ role: 'queue', cmd: 'start' })
-  })
+    it('should stop a worker', function (done) {
+      var task = {
+        task: 'my task',
+        param: 42
+      }
 
-  it('should stop a worker', function (done) {
-    var task = {
-      task: 'my task',
-      param: 42
-    }
+      s.add({
+        task: 'my task'
+      }, function (args, cb) {
+        cb()
+        done(new Error('this should never be called'))
+      })
 
-    s.add({
-      task: 'my task'
-    }, function (args, cb) {
-      cb()
-      done(new Error('this should never be called'))
+      s.act({ role: 'queue', cmd: 'start' })
+      s.act({ role: 'queue', cmd: 'stop' })
+      s.act({ role: 'queue', cmd: 'enqueue', msg: task }, done)
     })
 
-    s.act({ role: 'queue', cmd: 'start' })
-    s.act({ role: 'queue', cmd: 'stop' })
-    s.act({ role: 'queue', cmd: 'enqueue', msg: task }, done)
-  })
+    it('should restart a worker', function (done) {
+      var task = {
+        task: 'my task',
+        param: 42
+      }
 
-  it('should restart a worker', function (done) {
-    var task = {
-      task: 'my task',
-      param: 42
-    }
+      s.add({
+        task: 'my task'
+      }, function (args, cb) {
+        expect(args).to.include(task)
+        cb()
+        done()
+      })
 
-    s.add({
-      task: 'my task'
-    }, function (args, cb) {
-      expect(args).to.include(task)
-      cb()
-      done()
+      s.act({ role: 'queue', cmd: 'start' })
+      s.act({ role: 'queue', cmd: 'stop' })
+      s.act({ role: 'queue', cmd: 'enqueue', msg: task })
+      s.act({ role: 'queue', cmd: 'start' })
     })
-
-    s.act({ role: 'queue', cmd: 'start' })
-    s.act({ role: 'queue', cmd: 'stop' })
-    s.act({ role: 'queue', cmd: 'enqueue', msg: task })
-    s.act({ role: 'queue', cmd: 'start' })
   })
 })
--- test/transport.test.js
+++ test/transport.test.js
@@ -12,109 +12,113 @@ var beforeEach = lab.beforeEach
 var afterEach = lab.afterEach
 
 describe('seneca queue', function () {
-  var client
-  var server1
-  var server2
+  describe('Through transport', function () {
+    var client
+    var server1
+    var server2
 
-  beforeEach(function (done) {
-    server1 = seneca({log: 'silent'})
-                .use(queue)
-                .listen(8081)
+    beforeEach(function startServer1 (done) {
+      server1 = seneca({ log: 'silent' })
+        .use(queue)
+        .listen(8081)
+        .ready(done)
+    })
 
-    server1.ready(function () {
-      server2 = seneca({log: 'silent'})
-                  .use(queue)
-                  .listen(8082)
+    beforeEach(function startServer2 (done) {
+      server2 = seneca({ log: 'silent' })
+        .use(queue)
+        .listen(8082)
+        .ready(done)
+    })
 
-      server2.ready(function () {
-        client = seneca({log: 'silent'})
-                  .client({ port: 8081, pin: 'cmd:enqueue-remote,queue:queue1' })
-                  .client({ port: 8082, pin: 'cmd:enqueue-remote,queue:queue2' })
-                  .use(queue, {
-                    queues: [
-                      'queue1',
-                      'queue2'
-                    ]
-                  })
-                  .ready(done)
+    beforeEach(function startClient (done) {
+      client = seneca({ log: 'silent' })
+        .client({ port: 8081, pin: 'cmd:enqueue-remote,queue:queue1' })
+        .client({ port: 8082, pin: 'cmd:enqueue-remote,queue:queue2' })
+        .use(queue, {
+          queues: [
+            'queue1',
+            'queue2'
+          ]
+        })
+        .ready(done)
+    })
+
+    afterEach(function (done) {
+      async.each([server1, server2, client], function (server, cb) {
+        server.close(cb)
+      }, done)
+    })
+
+    it('should process a task on the first worker', function (done) {
+      var task = {
+        task: 'my task',
+        param: 42
+      }
+
+      server1.add({
+        task: 'my task'
+      }, function (args, cb) {
+        expect(args).to.include(task)
+        cb()
+        done()
+      })
+
+      server2.add({
+        task: 'my task'
+      }, function (args, cb) {
+        cb()
+        done(new Error('this should never be called'))
+      })
+
+      server1.act({ role: 'queue', cmd: 'start' })
+      server2.act({ role: 'queue', cmd: 'start' })
+
+      client.act({ role: 'queue', cmd: 'enqueue', msg: task }, function (err) {
+        if (err) { return done(err) }
       })
     })
-  })
 
-  afterEach(function (done) {
-    async.each([server1, server2], function (server, cb) {
-      server.close(cb)
-    }, done)
-  })
-
-  it('should process a task on the first worker', function (done) {
-    var task = {
-      task: 'my task',
-      param: 42
-    }
-
-    server1.add({
-      task: 'my task'
-    }, function (args, cb) {
-      expect(args).to.include(task)
-      cb()
-      done()
-    })
-
-    server2.add({
-      task: 'my task'
-    }, function (args, cb) {
-      cb()
-      done(new Error('this should never be called'))
-    })
-
-    server1.act({ role: 'queue', cmd: 'start' })
-    server2.act({ role: 'queue', cmd: 'start' })
-
-    client.act({ role: 'queue', cmd: 'enqueue', msg: task }, function (err) {
-      if (err) { return done(err) }
-    })
-  })
-
-  it('should process a task on both workers', function (done) {
-    var task1 = {
-      task: 'my task',
-      param: 1
-    }
-    var task2 = {
-      task: 'my task',
-      param: 2
-    }
-    var count = 2
-
-    server1.add({
-      task: 'my task'
-    }, function (args, cb) {
-      expect(args).to.include(task1)
-      cb()
-      if (--count === 0) {
-        done()
+    it('should process a task on both workers', function (done) {
+      var task1 = {
+        task: 'my task',
+        param: 1
       }
-    })
-
-    server2.add({
-      task: 'my task'
-    }, function (args, cb) {
-      expect(args).to.include(task2)
-      cb()
-      if (--count === 0) {
-        done()
+      var task2 = {
+        task: 'my task',
+        param: 2
       }
-    })
+      var count = 2
 
-    server1.act({ role: 'queue', cmd: 'start' })
-    server2.act({ role: 'queue', cmd: 'start' })
+      server1.add({
+        task: 'my task'
+      }, function (args, cb) {
+        expect(args).to.include(task1)
+        cb()
+        if (--count === 0) {
+          done()
+        }
+      })
 
-    client.act({ role: 'queue', cmd: 'enqueue', msg: task1 }, function (err) {
-      if (err) { return done(err) }
-    })
-    client.act({ role: 'queue', cmd: 'enqueue', msg: task2 }, function (err) {
-      if (err) { return done(err) }
+      server2.add({
+        task: 'my task'
+      }, function (args, cb) {
+        expect(args).to.include(task2)
+        cb()
+        if (--count === 0) {
+          done()
+        }
+      })
+
+      server1.act({ role: 'queue', cmd: 'start' })
+      server2.act({ role: 'queue', cmd: 'start' })
+
+      client.act({ role: 'queue', cmd: 'enqueue', msg: task1 }, function (err) {
+        if (err) { return done(err) }
+      })
+      client.act({ role: 'queue', cmd: 'enqueue', msg: task2 }, function (err) {
+        if (err) { return done(err) }
+      })
     })
   })
 })
